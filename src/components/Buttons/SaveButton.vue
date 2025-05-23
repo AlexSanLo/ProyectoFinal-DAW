abb1ae806847f9f8f2eae9ef7d6addf540cedbfe
AlexSanLo <san.lope.alex@gmail.com>
2025-05-23 16:48:06 +0200

    Color SaveButton

--- src/components/Buttons/SaveButton.vue
+++ src/components/Buttons/SaveButton.vue
@@ -2,7 +2,7 @@
   <section>
     <button
       @click="saveData"
-      class="px-4 py-2 bg-[var(--color-blue-medium)] hover:bg-[var(--color-blue-strong)] text-[var(--color-white)] font-semibold rounded-lg shadow-md"
+      class="px-4 py-2 bg-[var(--color-blue-strong)] hover:bg-[var(--color-blue-strong)] text-[var(--color-white)] font-semibold rounded-lg shadow-md"
     >
       Guardar
     </button>
@@ -30,7 +30,7 @@ const saveData = () => {
     etiquetas: props.inputs.noteEtiquetas.trim(),
     description: props.inputs.noteDescription.trim(),
     priority: props.selectValue.trim(),
-    timestamp: now, // SIEMPRE la fecha actual
+    timestamp: now, 
   };
 
   if (props.inputs.index !== undefined && props.inputs.index !== null) {
